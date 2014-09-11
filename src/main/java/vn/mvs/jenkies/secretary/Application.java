a14ae2c88867867c49910e4e490f6f3c82ee381e
Bui Minh Tien <tienbm@mvcorp.vn>
2014-09-11 17:27:16 +0700

    ad

---
+++ src/main/java/vn/mvs/jenkies/secretary/Application.java
@@ -0,0 +1,11 @@
+package vn.mvs.jenkies.secretary;
+
+/**
+ * Created by tienbm on 11/09/2014.
+ */
+public class Application {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+}
